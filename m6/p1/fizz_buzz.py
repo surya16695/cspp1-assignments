973d682e1c7690f97f4d21a9333847d700213dcb
Surya <suryasaginala@msitprogram.net>
2018-08-04 12:33:55 +0530

    " -> 2 of 7 passed. pylint: 9.09/10 "

--- m6/p1/fizz_buzz.py
+++ m6/p1/fizz_buzz.py
@@ -14,8 +14,7 @@ def main():
         if i%3 == 0:
             print("Fizz")
         elif i%5 == 0:
-            print("Buzz")
-        elif: 
-            print(i)
+            print("Buzz") 
+        print(i)
 if __name__ == '__main__':
     main()
