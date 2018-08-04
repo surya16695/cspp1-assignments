1f336f65bd686f28c24d92d1e14689a9632be293
Surya <suryasaginala@msitprogram.net>
2018-08-04 12:14:19 +0530

    " -> 0 of 7 passed. pylint: 0/0 "

--- m6/p3/digit_product.py
+++ m6/p3/digit_product.py
@@ -1,6 +1,6 @@
 '''
 Given a  number int_input, find the product of all the digits
-example: 
+example:
     input: 123
     output: 6
 '''
@@ -14,7 +14,7 @@ def main():
     if int_input < 0:
         int_input = -1*(int_input)
     else:
-        for  char in best_1:
+        for   best_1:
             pro_duct = (int_input%10)*pro_duct
             int_input = int_input//10
     print(pro_duct)
