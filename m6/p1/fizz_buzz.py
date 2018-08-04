b4572d7bd8a8f6852607eff72c0f1b1610ebce60
Surya <suryasaginala@msitprogram.net>
2018-08-04 12:54:20 +0530

    " -> 0 of 7 passed. pylint: 0/0 "

--- m6/p1/fizz_buzz.py
+++ m6/p1/fizz_buzz.py
@@ -17,11 +17,11 @@ def main():
     while i <= num:
         if i%3 == 0:
             print("Fizz")
-        elif i%5 == 0:
+        else i%5 == 0:
             print("Buzz")
         elif i%3 == 0 and i%5 == 0:
-            print ("Fizz")
-            print ("Buzz")
+            print("Fizz")
+            print("Buzz")
         else:
             print(i)
         i = i+1
