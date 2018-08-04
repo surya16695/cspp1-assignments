317a9920d3d220f60c34a9d50ac27016d784d3cc
Surya <suryasaginala@msitprogram.net>
2018-08-04 11:44:26 +0530

    " -> 0 of 7 passed. pylint: 0/0 "

--- m6/p3/digit_product.py
+++ m6/p3/digit_product.py
@@ -12,7 +12,7 @@ def main():
     if int_input <0:
         int_input = abs(int_input)
     pro_duct = 1
-    for i < 0:
+    for int_input > 0 :
     	pro_duct = (int_input%10)*pro_duct
     	int_input = int_input//10
     print(pro_duct)
