d10697ff2cf07e7c421f03fa2999e96d83d848c8
Surya <suryasaginala@msitprogram.net>
2018-08-04 11:24:35 +0530

    " -> 0 of 7 passed. pylint: 0/0 "

--- m6/p2/special_char.py
+++ m6/p2/special_char.py
@@ -10,7 +10,7 @@ def main():
     str_input = input()
     s = ""
     for i in len(str_input):
-    	if str_input[i] in (!, @, #, $, %, ^, &, *):
+    	if str_input[i] in ( !, @, #, $, %, ^, &, *):
     	    s = s+" "
     	else:
     	    s = s+str_input[i]
