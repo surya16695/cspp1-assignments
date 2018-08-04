490354dfab692629f627acf084f4ba4e02917b53
Surya <suryasaginala@msitprogram.net>
2018-08-04 11:56:43 +0530

    " -> 0 of 7 passed. pylint: 0/0 "

--- m6/p3/digit_product.py
+++ m6/p3/digit_product.py
@@ -9,10 +9,10 @@ def main():
     Read any number from the input, store it in variable int_input.
     '''
     int_input = int(input())
+    pro_duct = 1
     if int_input <0:
         int_input = abs(int_input)
     else:
-    pro_duct = 1
     for  i in int_input:
     	pro_duct = (int_input%10)*pro_duct
     	int_input = int_input//10
