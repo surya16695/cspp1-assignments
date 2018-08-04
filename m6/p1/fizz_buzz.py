2beaef66df86841247d642761afd56ea1ce39fcd
Surya <suryasaginala@msitprogram.net>
2018-08-04 13:02:54 +0530

    " -> 4 of 7 passed. pylint: 10.00/10 "

--- m6/p1/fizz_buzz.py
+++ m6/p1/fizz_buzz.py
@@ -12,14 +12,14 @@ def main():
     i = 1
     if num == 0:
         print()
-    if num<0:
+    if num < 0:
         print()
     while i <= num:
         if i%3 == 0:
             print("Fizz")
         elif i%5 == 0:
             print("Buzz")
-        elif i%15 == 0 and i%3 == 0 and i%5 == 0:
+        elif i%3 == 0 and i%5 == 0:
             print("Fizz")
             print("Buzz")
         else:
