f511a96e1c432cbf8d812ce494aeaa9440352695
Surya <suryasaginala@msitprogram.net>
2018-08-04 12:05:57 +0530

    " -> 0 of 7 passed. pylint: 6.36/10 "

--- m6/p3/digit_product.py
+++ m6/p3/digit_product.py
@@ -9,6 +9,7 @@ def main():
     Read any number from the input, store it in variable int_input.
     '''
     int_input = int(input())
+    best_1 = str(int_input)
     pro_duct = 1
     if int_input <0:
         int_input = -1*(int_input)
