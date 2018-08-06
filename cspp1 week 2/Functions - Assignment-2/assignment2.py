66a30655ca57fa1d8978ec1ddd55e826ad11451e
Surya <suryasaginala@msitprogram.net>
2018-08-06 21:02:10 +0530

    " -> 10 of 10 passed. pylint: 7.73/10 "

--- cspp1 week 2/Functions - Assignment-2/assignment2.py
+++ cspp1 week 2/Functions - Assignment-2/assignment2.py
@@ -1,30 +1,31 @@
-# Assignment-2 - Paying Debt off in a Year
+""" Assignment-2 - Paying Debt off in a Year
+Now write a program that calculates the minimum fixed
+monthly payment needed in order pay off a credit card balance within 12 months.
+By a fixed monthly payment, we mean a single numbe
+which does not change each month, but instead is a constant amount that will be
+paid each month.
 
-# Now write a program that calculates the minimum fixed monthly payment needed in order pay off a credit card balance within 12 months.
-# By a fixed monthly payment, we mean a single number which does not change each month, but instead is a constant amount that will be
-# paid each month.
+In this problem, we will not be dealing with a minimum monthly payment rate.
 
-# In this problem, we will not be dealing with a minimum monthly payment rate.
-
-# The following variables contain values as described below:
-# balance - the outstanding balance on the credit card
-# annualInterestRate - annual interest rate as a decimal
-
-# The program should print out one line: the lowest monthly payment that will pay off all debt in under 1 year, for example:
-# Lowest Payment: 180
-
-# Assume that the interest is compounded monthly according to the balance at the end of the month (after the payment for that month is
-# made).
-# The monthly payment must be a multiple of $10 and is the same for all months. Notice that it is possible for the balance to become
-# negative using this payment scheme, which is okay. A summary of the required math is found below:
-# Monthly interest rate = (Annual interest rate) / 12.0
-# Monthly unpaid balance = (Previous balance) - (Minimum fixed monthly payment)
-# Updated balance each month = (Monthly unpaid balance) + (Monthly interest rate x Monthly unpaid balance)
-
-
-
+The following variables contain values as described below:
+balance - the outstanding balance on the credit card
+annualInterestRate - annual interest rate as a decimal
+the program should print out one line: the lowest monthly
+payment that will pay off all debt in under 1 year, for example:
+Lowest Payment: 180
+Assume that the interest is compounded monthly according
+to the balance at the end of the month (after the payment for that month is
+made).
+The monthly payment must be a multiple of $10 and is the
+same for all months. Notice that it is possible for the balance to become
+negative using this payment scheme, which is okay. A summary of the required math is found below:
+Monthly interest rate = (Annual interest rate) / 12.0
+Monthly unpaid balance = (Previous balance) - (Minimum fixed monthly payment)
+Updated balance each month = (Monthly unpaid balance) + (Monthly interest rate x Monthly unpaid balance)
+"""
 def payingDebtOffInAYear(bal_ance, annual_interestrate):
-    i=1
+    """def of bal"""
+    i = 1
     ba_l = bal_ance
     pa_y = 0
     while True:
@@ -39,12 +40,10 @@ def payingDebtOffInAYear(bal_ance, annual_interestrate):
         else:
             break
     return pa_y
-      
-
 def main():
     data = input()
     data = data.split(' ')
     data = list(map(float, data))
-    print("Lowest Payment:",str(payingDebtOffInAYear(data[0],data[1])))
+    print("Lowest Payment:",str(payingDebtOffInAYear(data[0], data[1])))
 if __name__== "__main__":
     main()
