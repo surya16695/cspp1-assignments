33b4d3aa0d519b0866e91e63688c8c313153045f
Surya <suryasaginala@msitprogram.net>
2018-08-06 22:38:31 +0530

    " -> 0 of 10 passed. pylint: 0/0 "

--- cspp1 week 2/Functions - Assignment-3/assignment3.py
+++ cspp1 week 2/Functions - Assignment-3/assignment3.py
@@ -44,13 +44,13 @@ def payingDebtOffInAYear(balance, annualInterestRate):
     high = (balance * ((1.0 + monthlyInterestRate)**12))/12.0
     epsilon = 0.05
     minPay = (high + low)/2.0
-    month = 1
+    month = 0
     def calculate(month, balance, minPay, monthlyInterestRate):
-        while month < 12:
-            unpaidBalance = balance - minPay
-            balance = unpaidBalance + (monthlyInterestRate * unpaidBalance)
-            month += 1
-        return balance   
+    while month < 12:
+        unpaidBalance = balance - minPay
+        balance = unpaidBalance + (monthlyInterestRate * unpaidBalance)
+        month += 1
+    return balance   
     while abs(balance) >= epsilon:
         balance = initBalance
         month = 0
