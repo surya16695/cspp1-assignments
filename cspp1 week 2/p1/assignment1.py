a404b95691bd399e47c76bff46ce5f35d94d2d7b
Surya <suryasaginala@msitprogram.net>
2018-08-07 17:57:44 +0530

    " -> 10 of 10 passed. pylint: 5.45/10 "

--- cspp1 week 2/p1/assignment1.py
+++ cspp1 week 2/p1/assignment1.py
@@ -1,23 +1,22 @@
-# Exercise: Assignment-1
+# Exercise:""" Assignment-1"""
 # Write a Python function, factorial(n), that takes
 #in one number and returns the factorial of given number.
 
 # This function takes in one number and returns one number.
 
 
-def fact_orial(n):
+def fact_orial(n_1):
     '''
-    n is positive Integer
+    n_1 is positive Integer
 
     returns: a positive integer, the factorial of n.
     '''
     # Your code here
     """doc string"""
-    if n <= 1:
+    if n_1 <= 1:
         return 1
-    else:
-        return n*fact_orial(n-1)
-    pass
+    return n_1*fact_orial(n_1 - 1)
+
 """ def calling the function"""
 def main():
     a_1 = input()
