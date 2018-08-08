0aaf3161dfb7a8e81841041ce0e7cca0ca207a03
Surya <suryasaginala@msitprogram.net>
2018-08-08 19:50:08 +0530

    " -> 0 of 9 passed. pylint: 7.37/10 "

--- cspp1 week 2/p2/assignment2.py
+++ cspp1 week 2/p2/assignment2.py
@@ -15,9 +15,10 @@ def get_guessed_word(secret_word, letters_guessed):
     # FILL IN YOUR CODE HERE...
     for _ in secret_word:
         for char in letters_guessed:
-            if secret_word != letters_guessed:
+            if secret_word == leters_guessed:
                 secret_word = secret_word.replace(char, "_")
         return secret_word
+
 
 def main():
     '''
