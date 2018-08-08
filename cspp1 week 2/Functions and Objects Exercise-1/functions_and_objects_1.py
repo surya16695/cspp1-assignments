5da2b7e48b0b312b1202179da68aa52ef5cdf752
Surya <suryasaginala@msitprogram.net>
2018-08-08 15:27:18 +0530

    f n obj

---
+++ cspp1 week 2/Functions and Objects Exercise-1/functions_and_objects_1.py
@@ -0,0 +1,23 @@
+#Exercise : Function and Objects Exercise-1
+#Implement a function that converts the given testList = [1, -4, 8, -9] into [1, 4, 8, 9]
+
+
+def apply_to_each(L, f):
+    for i in range (0, len(L)):
+        L[i] = f(L[i])
+        #return L
+
+#def abs(n):
+
+
+def main():
+    data = input()
+    data = data.split()
+    list1 = []
+    for j in data:
+        list1.append(int(j))
+    apply_to_each(list1, abs)
+    print(list1)
+
+if __name__ == "__main__":
+    main()
