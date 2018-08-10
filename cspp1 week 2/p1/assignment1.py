9ea929bd52681eec392005e9819f4757ed3eedc2
Surya <suryasaginala@msitprogram.net>
2018-08-10 19:46:54 +0530

    " -> 0 of 9 passed. pylint: 0.00/10 "

--- cspp1 week 2/p1/assignment1.py
+++ cspp1 week 2/p1/assignment1.py
@@ -26,7 +26,7 @@ def get_word_score(word, n):
     # dictionary of words and scores
     words = {("", 7): 0, ("it", 7): 4, ("was", 7): 18, ("scored", 7): 54, ("waybill", 7): 155, ("outgnaw", 7): 127, ("fork", 7): 44, ("fork", 4): 94}
     for (word, n) in words.keys():
-        score = get_Word_Score(word, n)
+        score = get_word_score(word, n)
         if score != words[(word, n)]:
             print ("FAILURE: test_getWordScore()")
             print ("\tExpected", words[(word, n)], "points but got '" + str(score) + "' for word '" + word + "', n=" + str(n))
