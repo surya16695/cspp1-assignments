a84c69d1676d5700255b427bcfe57c027dd9a01b
Surya <suryasaginala@msitprogram.net>
2018-08-11 10:28:45 +0530

    " -> 0 of 3 passed. pylint: 6.50/10 "

---
+++ cspp1 week 2/p1/create_social_network.py
@@ -0,0 +1,60 @@
+'''
+    Assignment-1 Create Social Network
+'''
+
+def create_social_network(data):
+    '''
+        The data argument passed to the function is a string
+        It represents simple social network data
+        In this social network data there are people following other people
+
+        Here is an example social network data string:
+        John follows Bryant,Debra,Walter
+        Bryant follows Olive,Ollie,Freda,Mercedes
+        Mercedes follows Walter,Robin,Bryant
+        Olive follows John,Ollie
+
+        The string has multiple lines and each line represents one person
+        The first word of each line is the name of the person
+        The second word is follows that separates the person from the followers
+        After the second word is a list of people separated by ,
+
+        create_social_network function should split the string on lines
+        then extract the person and the followers by splitting each line
+        finally add the person and the followers to a dictionary and
+        return the dictionary
+
+        Caution: watch out for trailing spaces while splitting the string.
+        It may cause your test cases to fail although your output may be right
+
+        Error handling case:
+        Return a empty dictionary if the string format of the data is invalid
+        Empty dictionary is not None, it is a dictionary with no keys
+    '''
+
+    # remove the pass below and start writing your code
+    dict_1 = {}
+    l_1 = []
+    d_1 = []
+    for word in data:
+        d_1 = data.split('')
+        for word in d_1:
+            l_1 = l_1.split('follows')
+            dict_1[d_1] = [l_1]
+    return dict_1
+
+def main():
+    '''
+        handling testcase input and printing output
+    '''
+    string = ''
+    lines = int(input())
+    for i in range(lines):
+        i += 1
+        string += input()
+        string += '\n'
+
+    print(create_social_network(string))
+
+if __name__ == "__main__":
+    main()
