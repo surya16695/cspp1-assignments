af74970cc7810bdfe5cf12374ccb963883061888
Surya <suryasaginala@msitprogram.net>
2018-08-11 11:11:22 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- cspp1 week 2/p1/create_social_network.py
+++ cspp1 week 2/p1/create_social_network.py
@@ -39,13 +39,15 @@ def create_social_network(data):
     str_2 = ""
     for word in  data:
         str_1 = data.split()
+        print (str_1)
         for word in str_1:
             str_2 = str_1.split('follows')
+            print (str_2)
             i = 0
             for i in len(str_2):
                 dict_1(str_2[i]) = str_2[i+1].split(",")
                 dict_2 = dict_1
-    return dict_2
+            return dict_2
 
 
  
