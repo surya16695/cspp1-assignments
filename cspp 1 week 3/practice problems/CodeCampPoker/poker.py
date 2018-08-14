c48f63e30e3673083a0c5b6e478c2a83a3dcbc06
Surya <suryasaginala@msitprogram.net>
2018-08-14 22:31:07 +0530

    " -> 0 of 3 passed. pylint: 7.39/10 "

--- cspp 1 week 3/practice problems/CodeCampPoker/poker.py
+++ cspp 1 week 3/practice problems/CodeCampPoker/poker.py
@@ -81,16 +81,15 @@ def hand_rank(hand):
     # the second best would be a flush with the return value 2
     # third would be a straight with the return value 1
     # any other hand would be the fourth best with the return value 0
-    # max in poker function uses these return values to select the best hand
-    for each_element in hand:
-        if is_straight(hand) and is_flush(hand):
-            return 3
-        elif is_straight(hand):
-            return 1
-        elif is_flush(hand):
-            return 2
-        else:
-            return
+    # max in poker function uses these return values to select the best han
+    if is_straight(hand) and is_flush(hand):
+        return 3
+    elif is_straight(hand):
+        return 1
+    elif is_flush(hand):
+        return 2
+    else:
+        return
 
 
 
