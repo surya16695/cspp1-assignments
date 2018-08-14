73c3295eaa56900c682702041cb9667f014e8870
Surya <suryasaginala@msitprogram.net>
2018-08-14 12:40:06 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- cspp 1 week 3/practice problems/CodeCampPoker/poker.py
+++ cspp 1 week 3/practice problems/CodeCampPoker/poker.py
@@ -8,17 +8,17 @@ dec_k = ['AD', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', 'TD', 'KD', 'JD',
          'AH', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', 'TH', 'KH', 'JH', 'QH', 
          'AS', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', 'TS', 'KS', 'JS', 'QS', 
          'AC', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', 'TC', 'KC', 'JC', 'QC']
-    for i in range(len(hand)):
-        if dec_k[i][0] == 'A':
-            return 14
-        if dec_k[i][0] == 'K':
-            return 13
-        if dec_k[i][0] == 'Q':
-            return 12
-        if dec_k[i][0] == 'J':
-            return 11
-        if dec_k[i][0] == 'T':
-            return 10
+for i in range(len(hand)):
+    if dec_k[i][0] == 'A':
+        return 14
+    if dec_k[i][0] == 'K':
+        return 13
+    if dec_k[i][0] == 'Q':
+        return 12
+    if dec_k[i][0] == 'J':
+        return 11
+    if dec_k[i][0] == 'T':
+        return 10
 def is_straight(hand):
     '''
         How do we find out if the given hand is a straight?
