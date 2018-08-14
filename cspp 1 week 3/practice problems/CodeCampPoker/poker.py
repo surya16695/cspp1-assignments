600b1e7e2dd21a2cc482faacabd53b7438598f50
Surya <suryasaginala@msitprogram.net>
2018-08-14 13:01:34 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- cspp 1 week 3/practice problems/CodeCampPoker/poker.py
+++ cspp 1 week 3/practice problems/CodeCampPoker/poker.py
@@ -7,17 +7,19 @@ dec_k = ['AD', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', 'TD', 'KD', 'JD',
          'AH', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', 'TH', 'KH', 'JH', 'QH', 
          'AS', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', 'TS', 'KS', 'JS', 'QS', 
          'AC', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', 'TC', 'KC', 'JC', 'QC']
-for i in range(len(hand)-1):
-    if hand[i][0] == 'A':
-        return 14
-    elif hand[i][0] == 'K':
-        return 13
-    elif hand[i][0] == 'Q':
-        return 12
-    elif hand[i][0] == 'J':
-        return 11
-    elif hand[i][0] == 'T':
-        return 10
+    # for i in range(len(hand)-1):
+    def get_val(x):
+        if x[0] == 'A':
+            return 14
+        elif x[0] == 'K':
+            return 13
+        elif x[0] == 'Q':
+            return 12
+        elif x[0] == 'J':
+            return 11
+        elif x[0] == 'T':
+            return 10
+        return int(x)
 def is_straight(hand):
     '''
         How do we find out if the given hand is a straight?
@@ -28,11 +30,11 @@ def is_straight(hand):
         Think of an algorithm: given the card face value how to check if it a straight
         Write the code for it and return True if it is a straight else return False
     '''
+    hand_temp = sorted(hand, key=get_val)
     for i  in range (len(hand)):
-        if (int(hand[i][0])+1) == (int(hand[i+1][0])):
-            return True
-        else:
+        if (get_val(hand_temp[i])+1) != (get_val(hand_temp[i+1])):
             return False
+    return True
 
 
 def is_flush(hand):
