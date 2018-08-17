2e6816426eac50a258782fb8b900f8d1f32eba4a
Surya <suryasaginala@msitprogram.net>
2018-08-17 12:44:30 +0530

    " -> 0 of 3 passed. pylint: 5.20/10 "

--- cspp 1 week 3/practice problems/CodeCampDocumentDistance/document_distance.py
+++ cspp 1 week 3/practice problems/CodeCampDocumentDistance/document_distance.py
@@ -6,30 +6,11 @@ def similarity(dict1, dict2):
     '''
         Compute the document distance as given in the PDF
     '''
+    # dict1_temp = dict1.lower()
+    # dict2_temp = dict2.lower()
     word_stop = load_stopwords('stopwords.txt')
-    input_1 = []
-    input_2 = []
-    input_1 = dict1.lower()
-    input_2 = dict2.lower()
-    input_1 = input_1.strip().replace('.', '').replace(',', '').replace('?', '').split()
-    #print(input_1)
-    input_2 = input_2.strip().replace('.', '').replace(',', '').replace('?', '').split()
-    #print(input_2)
-    # dict1 = make_dict(input_1)
-    # dict2 = make_dict(input_2)
-    # dict_temp1 = {}
-    # dict_temp2 = {}
-    # stop_words = load_stopwords(r)
-    # dict_temp1 = del.dict1[stop_words]
-    # print(dict_temp1)
-    # dict_temp2 = del.dict2[stop_words]
-    # print(dict_temp2)
-    input_up1 = []
-    input_up2 = []
-    input_up1 = del input_1[word_stop]
-    print(input_1)
-    input_up2 = del input_2[word_stop]
-    print(input_2)
+    dict2_temp
+
 
 def load_stopwords(filename):
     '''
@@ -47,6 +28,7 @@ def make_dict(input):
             dict_1[word] = 1
         else:
             dict_1[word] += 1
+    print(dict_1)
     return dict_1
 def main():
     '''
@@ -54,8 +36,9 @@ def main():
     '''
     input1 = input()
     input2 = input()
-
-    print(similarity(input1, input2))
+    input_1 = make_dict(input1.lower())
+    input_2 = make_dict(input2.lower())
+    print(similarity(input_1, input_2))
 
 if __name__ == '__main__':
     main()
