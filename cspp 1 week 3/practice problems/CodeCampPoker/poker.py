a36637af6acdfa4a7eda90736e4b2b5f33163f9f
Surya <suryasaginala@msitprogram.net>
2018-08-17 10:14:22 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- cspp 1 week 3/practice problems/CodeCampPoker/poker.py
+++ cspp 1 week 3/practice problems/CodeCampPoker/poker.py
@@ -8,17 +8,18 @@ dec_k = ['AD', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', 'TD', 'KD', 'JD',
          'AS', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', 'TS', 'KS', 'JS', 'QS',
          'AC', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', 'TC', 'KC', 'JC', 'QC']
     # for i in range(len(hand)-1):
-#def get_val(x):
-#    if x[0] == 'A':
-#        return 14
-#    elif x[0] == 'K':
-#    elif x[0] == 'Q':
-#        return 12
-#    elif x[0] == 'J':
-#        return 11
-#    elif x[0] == 'T':
-#        return 10
-#    return int(x[0])
+def get_val(x):
+    if x[0] == 'A':
+        return 14
+    elif x[0] == 'K':
+        return 13
+    elif x[0] == 'Q':
+        return 12
+    elif x[0] == 'J':
+        return 11
+    elif x[0] == 'T':
+        return 10
+    return int(x[0])
 #def is_fullhouse(hand):
     #hand_temp_2 = sorted(hand, key=get_val)
     #for i in range (j=hand_temp_2):
@@ -56,15 +57,15 @@ dec_k = ['AD', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', 'TD', 'KD', 'JD',
     #return False
 
 #def is_straight(hand):
-    '''
-        How do we find out if the given hand is a straight?
-        The hand has a list of cards represented as strings.
-        There are multiple ways of checking if the hand is a straight.
-        Do we need both the characters in the string? No.
-        The first character is good enough to determine a straight
-        Think of an algorithm: given the card face value how to check if it a straight
-        Write the code for it and return True if it is a straight else return False
-    '''
+'''
+    How do we find out if the given hand is a straight?
+    The hand has a list of cards represented as strings.
+    There are multiple ways of checking if the hand is a straight.
+    Do we need both the characters in the string? No.
+    The first character is good enough to determine a straight
+    Think of an algorithm: given the card face value how to check if it a straight
+    Write the code for it and return True if it is a straight else return False
+'''
     #hand_temp = sorted(hand, key=get_val)
     #print(hand_temp)
     #for i  in range (len(hand_temp)-1):
@@ -86,6 +87,7 @@ dec_k = ['AD', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', 'TD', 'KD', 'JD',
     #    if hand[i][1] != (hand[i+1][1]):
     #        return False
     #return True
+
 #def hand_rank(hand):
     '''
         You will code this function. The goal of the function is to
