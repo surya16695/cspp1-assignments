4dd975435f1ddafa7726a04af771c94ecf62bd04
Surya <suryasaginala@msitprogram.net>
2018-08-17 11:49:28 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- cspp 1 week 3/practice problems/CodeCampDocumentDistance/document_distance.py
+++ cspp 1 week 3/practice problems/CodeCampDocumentDistance/document_distance.py
@@ -6,8 +6,27 @@ def similarity(dict1, dict2):
     '''
         Compute the document distance as given in the PDF
     '''
-    dict1_temp = dict1.lower()
-    dict2_temp = dict2.lower()
+    # dict1_temp = dict1.lower()
+    # dict2_temp = dict2.lower()
+    input_1 = input1.lower()
+    input_2 = input2.lower()
+    #print(input1)
+    #print(input2)
+    dict1 = {}
+    dict2 = {}
+    input_1 = input_1.strip().replace('.', '').replace(',', '').replace('?', '').split()
+    #print(input_1)
+    input_2 = input_2.strip().replace('.', '').replace(',', '').replace('?', '').split()
+    #print(input_2)
+    dict1 = make_dict(input_1)
+    # print(dict1)
+    dict2 = make_dict(input_2)
+    # print(dict2)
+    stop_words = load_stopwords(r)
+    dict_temp1 = del.dict1[stop_words]
+    print(dict_temp1)
+    dict_temp2 = del.dict2[stop_words]
+    print(dict_temp2)
 
 def load_stopwords(filename):
     '''
@@ -32,20 +51,7 @@ def main():
     '''
     input1 = input()
     input2 = input()
-    input_1 = input1.lower()
-    input_2 = input2.lower()
-    #print(input1)
-    #print(input2)
-    dict1 = {}
-    dict2 = {}
-    input_1 = input_1.strip().replace('.', '').replace(',', '').replace('?', '').split()
-    #print(input_1)
-    input_2 = input_2.strip().replace('.', '').replace(',', '').replace('?', '').split()
-    #print(input_2)
-    dict1 = make_dict(input_1)
-    print(dict1)
-    dict2 = make_dict(input_2)
-    print(dict2)
+
     print(similarity(input1, input2))
 
 if __name__ == '__main__':
