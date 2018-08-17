cbb1e6329007a2092596b51cf7e836401313a999
Surya <suryasaginala@msitprogram.net>
2018-08-17 10:12:42 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- cspp 1 week 3/practice problems/CodeCampPoker/poker.py
+++ cspp 1 week 3/practice problems/CodeCampPoker/poker.py
@@ -4,23 +4,21 @@
     https://en.wikipedia.org/wiki/List_of_poker_hands
 '''
 dec_k = ['AD', '2D', '3D', '4D', '5D', '6D', '7D', '8D', '9D', 'TD', 'KD', 'JD', 'QD'
-         'AH', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', 'TH', 'KH', 'JH', 'QH', 
-         'AS', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', 'TS', 'KS', 'JS', 'QS', 
+         'AH', '2H', '3H', '4H', '5H', '6H', '7H', '8H', '9H', 'TH', 'KH', 'JH', 'QH',
+         'AS', '2S', '3S', '4S', '5S', '6S', '7S', '8S', '9S', 'TS', 'KS', 'JS', 'QS',
          'AC', '2C', '3C', '4C', '5C', '6C', '7C', '8C', '9C', 'TC', 'KC', 'JC', 'QC']
     # for i in range(len(hand)-1):
-def get_val(x):
-    if x[0] == 'A':
-        return 14
-    elif x[0] == 'K':
-        return 13
-    elif x[0] == 'Q':
-        return 12
-    elif x[0] == 'J':
-        return 11
-    elif x[0] == 'T':
-        return 10
-    return int(x[0])
-    
+#def get_val(x):
+#    if x[0] == 'A':
+#        return 14
+#    elif x[0] == 'K':
+#    elif x[0] == 'Q':
+#        return 12
+#    elif x[0] == 'J':
+#        return 11
+#    elif x[0] == 'T':
+#        return 10
+#    return int(x[0])
 #def is_fullhouse(hand):
     #hand_temp_2 = sorted(hand, key=get_val)
     #for i in range (j=hand_temp_2):
@@ -88,9 +86,6 @@ def get_val(x):
     #    if hand[i][1] != (hand[i+1][1]):
     #        return False
     #return True
-
-    
-
 #def hand_rank(hand):
     '''
         You will code this function. The goal of the function is to
