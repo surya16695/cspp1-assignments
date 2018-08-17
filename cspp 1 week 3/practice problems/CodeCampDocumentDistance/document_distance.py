229a4c901f56631c1fe8ea6d5a2579e55e167baa
Surya <suryasaginala@msitprogram.net>
2018-08-17 17:25:01 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- cspp 1 week 3/practice problems/CodeCampDocumentDistance/document_distance.py
+++ cspp 1 week 3/practice problems/CodeCampDocumentDistance/document_distance.py
@@ -1,6 +1,20 @@
 '''
     Document Distance - A detailed description is given in the PDF
 '''
+import re
+
+def clean(input_string):
+    input_string = input1.lower()
+    regex = re.compile('(^a-z)')
+    input_string = regex.sub('', input_string)
+    return input_string
+
+    
+def computation(dict):
+    numerator = sum(values[0]*values[1] for value in dict.values())
+    denoinator1 = math.sqrt(sum(value[0]**2 for value in dict.values()))
+    denoinator2 = math.sqrt(sum(value[0]**2 for value in dict.values()))
+    return (numerator/(denominator1*denominator2))
 
 def similarity(dict1, dict2):
     '''
@@ -8,13 +22,33 @@ def similarity(dict1, dict2):
     '''
     #dict1 = make_dict(input_1)
     #dict2 = make_dict(input_2)
-    dict_1 = ""
-    dict_2 = ""
+    #dict1 = dict1.strip().replace('.', '').replace(',', '').replace('?', '').split()
+    #print(input_1)
+    #dict2 = dict2.strip().replace('.', '').replace(',', '').replace('?', '').split()
+    #print(input_2)
+
+    dict_1 = make_dict(dict1)
+    dict_2 = make_dict(dict2)
     word_stop = load_stopwords('stopwords.txt')
-    dict_1 = dict1 - word_stop.keys()
-    print(dict_1)
-    dict_2 = dict2 - word_stop.keys()
-    print(dict_2)
+    dict_temp1 = Delete(dict_1, word_stop)
+    dict_temp2 = Delete(dict_2, word_stop)
+    dict_temp3 = adding(dict_temp1, dict_temp2)
+    return computation(dict_temp3)
+
+    
+def adding(dict1, dict2):
+    for word in dict1:
+        if word in dict2:
+            dict_t[word] = [dict1.get(word), dict2.get(word)] 
+    return dict_t
+
+
+def Delete(dict_a, dict_b):
+    for word in dict_a:
+        if word in dict_b:
+            del dict_a(word)
+    return dict_a
+
 
 def load_stopwords(filename):
     '''
@@ -25,6 +59,8 @@ def load_stopwords(filename):
         for line in filename:
             stopwords[line.strip()] = 0
     return stopwords
+
+
 def make_dict(input):
     dict_A = {}
     for word in input:
@@ -40,7 +76,6 @@ def main():
     '''
     input1 = input()
     input2 = input()
-    input_1 = input1.lower()
     input_2 = input2.lower()
     print(similarity(input_1, input_2))
 
