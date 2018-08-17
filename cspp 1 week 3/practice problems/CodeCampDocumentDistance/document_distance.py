71fef909354e1925f7a6c8037e2e52e7ab79ca3c
Surya <suryasaginala@msitprogram.net>
2018-08-17 11:22:18 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- cspp 1 week 3/practice problems/CodeCampDocumentDistance/document_distance.py
+++ cspp 1 week 3/practice problems/CodeCampDocumentDistance/document_distance.py
@@ -18,21 +18,33 @@ def load_stopwords(filename):
         for line in filename:
             stopwords[line.strip()] = 0
     return stopwords
-
+def make_dict(input)
+	for word in input:
+    	if word not in dict1:
+    		dict1[word] = 1
+    	else :
+    		dict1[word] += 1
+    return dict1
 def main():
     '''
         take two inputs and call the similarity function
     '''
     input1 = input()
     input2 = input()
-    input1 = input1.lower()
-    input2 = input2.lower()
+    input_1 = input1.lower()
+    input_2 = input2.lower()
     #print(input1)
     #print(input2)
-    input_1 = input1.strip().replace('.', '').replace(',', '').replace('?', '').split()
-    print(input_1)
-    input_2 = input2.strip().replace('.', '').replace(',', '').replace('?', '').split()
-    print(input_2)
+    dict1 = {}
+    dict2 = {}
+    input_1 = input_1.strip().replace('.', '').replace(',', '').replace('?', '').split()
+    #print(input_1)
+    input_2 = input_2.strip().replace('.', '').replace(',', '').replace('?', '').split()
+    #print(input_2)
+    dict1 = make_dict(input_1)
+    print(dict1)
+    dict2 = make_dict(input_2)
+    print(dict2)
     print(similarity(input1, input2))
 
 if __name__ == '__main__':
