7abbfbc33b3639729b0d1f6353c17b4695f4d648
Surya <suryasaginala@msitprogram.net>
2018-08-18 11:21:25 +0530

    " -> 0 of 1 passed. pylint: 0/0 "

--- p1/build_search_index.py
+++ p1/build_search_index.py
@@ -78,12 +78,12 @@ def build_search_index(docs):
     dic_2 = make_dict(documents)
     dic_3 = word_list(dic_2)
     dic_4 = computing(dic_3)
-    for i in range dic_4.key():
+    for i in dic_4.keys():
         coun_t = 0
         for  word in dic_4[i]:
             if word in dic_4.values():
                 coun_t += 1
-            return word = [(key(), coun_t)]
+            return word = [i, coun_t)]
 
 
 
