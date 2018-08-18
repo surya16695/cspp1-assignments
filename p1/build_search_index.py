8b891ea3d9ffc7a2006081db18aed7545b032d81
Surya <suryasaginala@msitprogram.net>
2018-08-18 11:23:02 +0530

    " -> 0 of 1 passed. pylint: 6.96/10 "

--- p1/build_search_index.py
+++ p1/build_search_index.py
@@ -123,10 +123,10 @@ def main():
     for i in range(lines):
         documents.append(input())
         i += 1
-    print(documents)
+    #print(documents)
  
     # call print to display the search index
-    #print_search_index(build_search_index(documents))
+    print_search_index(build_search_index(documents))
 
 if __name__ == '__main__':
     main()
