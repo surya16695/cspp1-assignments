00b353572458b3da2a21ea6c1f4aeded477b895b
Surya <suryasaginala@msitprogram.net>
2018-08-18 10:31:47 +0530

    " -> 0 of 1 passed. pylint: 0/0 "

--- p1/build_search_index.py
+++ p1/build_search_index.py
@@ -60,7 +60,7 @@ def computing(dict):
         for word in dict[key]:
             if word not in word_s:
                 if word not in dict:
-                    dict[key] = dict[key].remove(word)
+                    dict[key] = del dict[key](word)
     print(dict)
     return dict
 def build_search_index(docs):
