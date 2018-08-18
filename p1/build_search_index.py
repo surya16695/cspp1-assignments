0f2dbe1a87191ed8974bbdb87a77689bea81fb4a
Surya <suryasaginala@msitprogram.net>
2018-08-18 10:51:37 +0530

    " -> 0 of 1 passed. pylint: 8.20/10 "

--- p1/build_search_index.py
+++ p1/build_search_index.py
@@ -60,7 +60,7 @@ def computing(dict):
     for key in dict:
         for word in dict[key]:
             if word in word_s:
-                dict[key] = dict[key].replace(word, '')
+                dict[key] = dict[key] - word_s
     print(dict)
     return dict
 def build_search_index(docs):
