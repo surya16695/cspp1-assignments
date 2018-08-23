5c45a30145fec7f3615b6a21f039b63de1499611
Surya <suryasaginala@msitprogram.net>
2018-08-23 11:59:18 +0530

    " -> 0 of 8 passed. pylint: 0/0 "

--- mod 21/matrix_operations.py
+++ mod 21/matrix_operations.py
@@ -17,11 +17,13 @@ def add_matrix(m1, m2):
         error message should be "Error: Matrix shapes invalid for addition"
     '''
     if len(m1) == len(m2) and len(m1[0]) == len(m2[0]):
-        sum = []
+        sum = 0
+        result = []
         for i in range(len(m1)-1):
             for j in range(len(m1[i]-1)):
-                sum = sum + sum[i][j].append(m1[i][j] + m2[i][j])
-        return sum
+            sum = (m1[i][j] + m2[i][j])
+            result[i].append(sum)
+        return result
     return print("Error: Matrix shapes invalid for addition")
 
 def read_matrix():
