642850588e4f7a75888139f05751aaae74e2b2ed
Surya <suryasaginala@msitprogram.net>
2018-08-24 21:55:44 +0530

    " -> 14 of 14 passed. pylint: 9.57/10 "

--- mod 21/tic tac toe/tic_tac_toe.py
+++ mod 21/tic tac toe/tic_tac_toe.py
@@ -1,70 +1,96 @@
-''' Write a python program to announce the winner of a tic tac toe game.
-  You can try playing the game here. ​https://www.google.com/search?q=tic+tac+toe'''
-  #steps to be followed
-def row_operation(tic_1):
-	count_1 = 0
-	a = 0
-	for i in range (len(tic_1)):
-		count = 0
-		for j in range(len(tic_1[0])):
-			if tic_1[i][i] == tic_1[i][j]:
-				count += 1
-			print(count)
-		if count == 3:
-			count_1 += 1
-			a = i
-		print(count_1)
-	if count_1 == 1:
-		return(tic_1[a])
-	return None
-	len_t = tic[i].count(tic[1][1])
-def column_operation(tic_1):
-	count_1 = 0
-	a = 0
-	b = 0
-	for i in range (len(tic_1)):
-		count = 0
-		for j in range(1, len(tic_1[0])):
-			if tic_1[j-1][i] == tic_1[j][i]:
-				count += 1
-			print(count)
-		if count == 2:
-			count_1 += 1
-			a = j-1
-			b = i
-		print(count_1)
-	if count_1 == 1:
-		return(tic_1[a][b])
-	return None
-# def diagonal_operation(tic_1):
-# 	count_1 = 0
+'''
+Author: Prudhvik
+Date: 24-08-2018
+'''
+
+def is_valid_game(game_grid):
+    '''check's whether game isvalid'''
+    x_count, o_count = 0, 0
+    for each_row in game_grid:
+        for each_box in each_row:
+            if each_box == 'x':
+                x_count += 1
+            elif each_box == 'o':
+                o_count += 1
+            elif each_box == '.':
+                pass
+            else:
+                return "invalid input"
+    if abs(o_count - x_count) != 1:
+        return "invalid game"
+
+    return None
+
+def horizontal_check(game_grid):
+    '''checks for horizontal conditions'''
+    for each_row in game_grid:
+        if each_row.count(each_row[0]) == 3 and each_row[0] != '.':
+            return each_row[0]
+    return None
+
+def vertical_check(game_grid):
+    '''checks vertical'''
+    win_count = 0
+    win_flag = game_grid[0][0]
+    grid_length = len(game_grid)
+    for i in range(grid_length):
+        if win_count == 3:
+            break
+        win_count = 0
+        win_flag = game_grid[0][i]
+        if win_flag == '.':
+            continue
+        for j in range(grid_length):
+            if game_grid[j][i] == win_flag:
+                win_count += 1
+            else:
+                break
+    if win_count == 3:
+        return win_flag
+    return None
+
+def diagonal_left_to_right(game_grid):
+    '''checks diagonal left to right'''
+    grid_length = len(game_grid)
+    win_flag = game_grid[0][0]
+    win_count = 0
+    if win_flag != '.':
+        for i in range(grid_length):
+            if game_grid[i][i] == win_flag:
+                win_count += 1
+            else:
+                break
+    if win_count == 3:
+        return win_flag
+    return None
+
+def diagonal_right_to_left(game_grid):
+    '''checks diagonal right to left'''
+    grid_length = len(game_grid)
+    win_flag = game_grid[0][-1]
+    win_count = 0
+    if win_flag != '.':
+        for i in range(grid_length):
+            if game_grid[i][-i-1] == win_flag:
+                win_count += 1
+            else:
+                break
+    if win_count == 3:
+        return win_flag
+    return None
+
+def play_game(game_grid):
+    '''its playing game'''
+    return is_valid_game(game_grid) or horizontal_check(game_grid) or \
+    vertical_check(game_grid) or diagonal_left_to_right(game_grid) or \
+    diagonal_right_to_left(game_grid) or "draw"
 
 
+def main():
+    GRID_LINES = 3
+    INPUT_GRID = []
+    for _ in range(GRID_LINES):
+        INPUT_GRID.append(input().split())
+    print(play_game(INPUT_GRID))
 
-
-
-
-
-
-
-
-n = 3
-tic_1 = []
-tic_2 = []
-tic_3 = []
-tic_4 = []
-for i in range(0, n, 1):
-	line = input().split()
-	tic_1.append(line)
-print (tic_1)
-# tic_2.append(row_operation(tic_1))
-# print(tic_2)
-# if tic_2 != None:
-# 	print ("winner is:", tic_2[0][0])
-tic_3.append(column_operation(tic_1))
-print(tic_3)
-if tic_3 != None:
-	print ("winner is:", tic_3[0])
-
-
-
+main()
