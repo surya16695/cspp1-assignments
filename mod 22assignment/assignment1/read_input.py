1dacb152aabdec85b0b26d40a39d4f429a64b174
Surya <suryasaginala@msitprogram.net>
2018-08-25 10:47:08 +0530

    " -> 0 of 2 passed. pylint: 0/0 "

--- mod 22assignment/assignment1/read_input.py
+++ mod 22assignment/assignment1/read_input.py
@@ -3,15 +3,20 @@ Write a python program to read multiple lines of text input and store the input 
 '''
 
 def main():
-    n = int(input())
-    lines = input().split()
-    s = ""
-    for i in range(n):
-        for line in lines:
-            s = s +'_'+ str(line)
-    print(s)
-    # return s
+    # n = int(input())
+    # lines = input().split()
+    # s = ""
+    # for i in range(n):
+    #     for line in lines:
+    #         s = s + str(line)
+    # print(s)
+    # # return s
+    no_of_lines = int(input())
+    lines = ""
+    for i in xrange(no_of_lines):
+        lines+=input()+"\n"
 
+    print lines
 
 if __name__ == '__main__':
     main()
