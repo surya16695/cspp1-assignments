f841adc6b291c521353b2371d34e03b31ab9dc03
Surya <suryasaginala@msitprogram.net>
2018-08-25 14:24:02 +0530

    " -> 0 of 4 passed. pylint: 0/0 "

--- mod 22assignment/assignment3/tokenize.py
+++ mod 22assignment/assignment3/tokenize.py
@@ -2,28 +2,28 @@
 Write a function to tokenize a given string and return a dictionary with the frequency of
 each word
 '''
-import re
+mport re
 
 def tokenize(string):
-    """tokenize strings"""
-    dic_t = {}
-    # s = ""
-    input_string = string    
-    regex = re.compile("[^a-zA-Z0-9]")
-    input_string = regex.sub('', input_string)
-    s = input_string.split(" ")[:-1]
-    for word in s:
-        if word not in dic_t:
-            dic_t[word] = string.count(word) 
-    return dic_t
-    
+   '''Converting given strings to a dictionary with frequencies of each word'''
+   reg = re.compile("[^a-z A-Z 0-9]")
+   token_dict = {}
+   line = []
+   for each_line in string:
+       line.append(reg.sub('', each_line).split())
+
+   for each_str in line:
+       for each_word in each_str:
+           token_dict[each_word] = token_dict.get(each_word, 0) + 1
+   return token_dict
+
 def main():
-    n = int(input())
-    l_1 = ""
-    for i in range(n):
-        l_1 += input()
-        i = i+1
-    # print(l_1)
-    print(tokenize(l_1.split()))
+   '''Taking inputs and displaying outputs'''
+   num_lines = int(input())
+   list_ofstrs = []
+   for _ in range(num_lines):
+       list_ofstrs.append(input())
+   # print(list_ofstrs)
+   print(tokenize(list_ofstrs))
 if __name__ == '__main__':
-    main()
+   main()
