c375cc71777b949b1a24c463e8a11ff7e9b128b6
Surya <suryasaginala@msitprogram.net>
2018-08-25 11:30:43 +0530

    " -> 0 of 4 passed. pylint: 0/0 "

--- mod 22assignment/assignment3/tokenize.py
+++ mod 22assignment/assignment3/tokenize.py
@@ -10,6 +10,7 @@ def tokenize(string):
         for word in string:
             if word in dic_t:
                 count_1 += 1
+            else:
             dic_t[word] = count_1
     return dic_t
             
