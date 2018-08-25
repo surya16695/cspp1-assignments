ef3c942e0026ee01dad46db6f3d526a7dc5fb76c
Surya <suryasaginala@msitprogram.net>
2018-08-25 10:57:04 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

---
+++ mod 22assignment/assignment2/clean_input.py
@@ -0,0 +1,18 @@
+'''
+Write a function to clean up a given string by removing the special characters and retain 
+alphabets in both upper and lower case and numbers.
+'''
+import re
+def clean_string(string):
+	input_string = input_string
+	regex = re.compile("[^a-z A-Z]")
+    input_string = regex.sub('', input_string)
+    # print(input_string)
+    return input_string.split()
+
+def main():
+    string = input()
+    print(clean_string(string))
+
+if __name__ == '__main__':
+    main()
