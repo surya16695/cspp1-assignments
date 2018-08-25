1038415a067f2e0725660d46fe2767f32420b4f2
Surya <suryasaginala@msitprogram.net>
2018-08-25 10:41:51 +0530

    " -> 0 of 2 passed. pylint: 5.00/10 "

--- mod 22assignment/assignment1/read_input.py
+++ mod 22assignment/assignment1/read_input.py
@@ -4,11 +4,11 @@ Write a python program to read multiple lines of text input and store the input 
 
 def main():
     n = int(input())
-    lines = input()
+    lines = input().split()
     s = ""
     for i in range(n):
         for line in lines:
-            s = s + str(line.split())
+            s = s + str(line)
     print(s)
     # return s
 
