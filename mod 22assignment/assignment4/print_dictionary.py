ee60d9e80a5ddc2d64fc835c93c3ba307ceb0643
Surya <suryasaginala@msitprogram.net>
2018-08-25 11:48:34 +0530

    " -> 0 of 3 passed. pylint: 4.44/10 "

--- mod 22assignment/assignment4/print_dictionary.py
+++ mod 22assignment/assignment4/print_dictionary.py
@@ -6,7 +6,7 @@ the key and frequency with a SPACE - SPACE.
 
 def print_dictionary(dictionary):
     for word in dictionary:
-    	print(word, dictionary[word])
+    	print(word - dictionary[word])
     return
 
 def main():
